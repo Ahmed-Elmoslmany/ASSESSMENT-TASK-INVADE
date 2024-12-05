c7734618015cd00c4db2e1ff10ba501dd00c9410
Ahmed-Elmoslmany <ahmedelmoslmany74@gmail.com>
2024-12-05 22:14:18 +0200

    feat/finish-read-and-create-endpoints✅

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Resources\TaskResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function index()
+    {
+       
+        $tasks = auth()->user()->tasks()->with('category')->paginate(10);
+        return TaskResource::collection($tasks);
+    }
+
+    public function store(StoreTaskRequest $request)
+    {   
+
+        // dd($request->validated());
+        $task = auth()->user()->tasks()->create($request->validated());
+        return new TaskResource($task);
+    }
 }
